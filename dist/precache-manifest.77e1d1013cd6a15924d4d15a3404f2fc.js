ba3ad9a8c635994d5850c0fc474c7d398ba2a5ef
roman.markov <roman.markov@fogstream.ru>
2020-07-14 14:52:47 +1000

    dep

---
+++ dist/precache-manifest.77e1d1013cd6a15924d4d15a3404f2fc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/.nojekyll"
+  },
+  {
+    "revision": "2e4fa68082acfe228fa4",
+    "url": "/css/app.d071f24a.css"
+  },
+  {
+    "revision": "cb2758d6d2273fb7250729c1406e1b92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e8381f390a2ddf79cd8",
+    "url": "/js/about.58db8936.js"
+  },
+  {
+    "revision": "2e4fa68082acfe228fa4",
+    "url": "/js/app.417d3990.js"
+  },
+  {
+    "revision": "8a232cf0a9545865884d",
+    "url": "/js/chunk-vendors.47e23e39.js"
+  },
+  {
+    "revision": "61623089c5a891fe2adfa3b5e5f8070e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "6db98a41154d141de4d62fb026f7ce34",
+    "url": "/offline.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
